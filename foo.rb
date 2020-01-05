4033c1e7eecb5467eadc27c0884da52c8e2d5b8d
Connor Shea <connor.james.shea@gmail.com>
2020-01-05 16:16:59 -0700

    Initial commit.
    Add Sorbet and foo.rb.

---
+++ foo.rb
@@ -0,0 +1,29 @@
+# typed: true
+extend T::Sig
+
+module I1
+  def i1; end
+end
+
+module I2
+  def i2; end
+end
+
+class C
+  include I1
+  include I2
+end
+
+class D
+  include I1
+  include I2
+end
+
+sig { params(x: T.all(I1, I2)).void}
+def foo(x)
+  x.i1
+  x.i2
+end
+
+foo(C.new)
+foo(D.new)
